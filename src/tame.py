ce91af3030d1523ab9703b13f30e32c983b9a6db
Jason Held <jsonheld@gmail.com>
2015-12-06 12:02:17 -0500

    re-enable POST db entrance, arg parse, db meter.

--- src/tame.py
+++ src/tame.py
@@ -2,6 +2,7 @@ import time
 import threading
 import json
 import traceback
+import argparse
 
 import bottle
 import alsaaudio
@@ -10,12 +11,18 @@ from soundmeter.monitor import Meter
 
 from math import log10
 
+driver_shared_data = {}
+
 def audio_meter(driver_shared_data):
     while True:
-        m = Meter(collect=True, seconds=2)
-        m.start()
-        m.stop()
-        driver_shared_data['db'] = 20 * log10(m._data['avg']/32678.0)
+        try:
+            m = Meter(collect=True, seconds=2)
+            m.start()
+            m.stop()
+            driver_shared_data['db'] = 20 * log10(m._data['avg']/(32678.0 - 32677.0))
+            print('metered db: {}'.format(driver_shared_data['db']))
+        except Exception:
+            pass
 
 def tame_driver(driver_shared_data=None):
     """
@@ -30,20 +37,22 @@ def tame_driver(driver_shared_data=None):
         # print('have data: {}'.format(driver_shared_data))
         adjustment_mode = driver_shared_data.get('mode', 'linear')
         preferred_audio_range = driver_shared_data.get('range', (-40, -30, ))
+        min_db, max_db = preferred_audio_range
         current_decibel_level = driver_shared_data.get('db', -75)
-        print('db from meter: {}'.format(current_decibel_level))
         current_volume = int(default_mixer.getvolume()[0])
         sweet_spot = float(sum(preferred_audio_range)) / len(preferred_audio_range)
         print('current_volume: {}'.format(current_volume))
+        print('preferred range: {}'.format(preferred_audio_range))
         try:
-            if current_decibel_level < sweet_spot and current_decibel_level >= sweet_spot - 15:
+            # TODO: reinstitute sweet_spot when ready.
+            # if current_decibel_level < sweet_spot and (current_decibel_level >= sweet_spot - 15 and current_decibel_level < preferred_audio_range[1] and preferred_audio_range[1] < preferred_audio_range[0]):
+            #     default_mixer.setvolume(current_volume + 3)
+            # if current_decibel_level == sweet_spot or current_decibel_level== sweet_spot - 2 or current_decibel_level == sweet_spot + 2:
+            #     time.sleep(2)
+            #     continue
+            if (current_decibel_level < max_db and max_db <= min_db) or (current_decibel_level > max_db and max_db >= min_db):
                 default_mixer.setvolume(current_volume + 3)
-            elif current_decibel_level == sweet_spot or current_decibel_level== sweet_spot - 2 or current_decibel_level == sweet_spot + 2:
-                time.sleep(2)
-                continue
-            elif current_decibel_level < preferred_audio_range[0]:
-                default_mixer.setvolume(current_volume + 3)
-            elif current_decibel_level > preferred_audio_range[1]:
+            elif (current_decibel_level < min_db and min_db <= max_db) or (current_decibel_level > min_db and min_db >= max_db):
                 default_mixer.setvolume(current_volume - 3)
         except alsaaudio.ALSAAudioError as e:
             print(e)
@@ -52,11 +61,9 @@ def tame_driver(driver_shared_data=None):
 @bottle.route('/', method='POST')
 @bottle.route('', method='POST')
 def index():
-    pass
-    # audio_data = json.loads(list(bottle.request.POST.keys())[0])
-    # print(audio_data)
-    # global driver_shared_data
-    # driver_shared_data['db'] = float(audio_data['value'])
+    audio_data = json.loads(list(bottle.request.POST.keys())[0])
+    global driver_shared_data
+    driver_shared_data['db'] = float(audio_data['value'])
 
 
 @bottle.route('/')
@@ -65,24 +72,44 @@ def index_get():
     upper_range = json.loads(bottle.request.GET.get('too_high', 'false'))
     if upper_range:
         global driver_shared_data
-        driver_shared_data['range'] = driver_shared_data.get('range', (-40, -30, ))[0], driver_shared_data['db'] - 20
+        new_range = driver_shared_data.get('range', (-40, -30, ))
+        new_value = driver_shared_data['db']
+        min_db, max_db = new_range
+        if min_db < max_db:
+            max_db = new_value - 5
+        elif min_db > max_db:
+            max_db = new_value + 5
+        new_range = min_db, max_db
+        driver_shared_data['range'] = new_range
     lower_range = json.loads(bottle.request.GET.get('too_low', 'false'))
     if lower_range:
         global driver_shared_data
-        driver_shared_data['range'] = driver_shared_data['db'] + 20, driver_shared_data.get('range', (-40, -30, ))[1]
+        new_range = driver_shared_data.get('range', (-40, -30, ))
+        new_value = driver_shared_data['db']
+        min_db, max_db = new_range
+        if max_db < min_db:
+            min_db = new_value - 5
+        elif max_db > min_db:
+            min_db = new_value + 5
+        new_range = min_db, max_db
+        driver_shared_data['range'] = new_range
     return json.dumps(driver_shared_data['db'])
 
 
-driver_shared_data = {}
 tame_thread = threading.Thread(target=tame_driver, kwargs=dict(driver_shared_data=driver_shared_data))
 tame_thread.daemon = True
-tame_thread.start()
 
 audio_thread = threading.Thread(target=audio_meter, args=(driver_shared_data, ))
 audio_thread.daemon = True
 audio_thread.start()
 
 if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--only-meter', type=bool, help='Whether or not we only want to meter the sound -- not provide control',
+                    default=False)
+    args = ap.parse()
+    if not args.only_meter:
+        tame_thread.start()
     bottle.run(host='0.0.0.0')
     # while True:
     #     time.sleep(2)
